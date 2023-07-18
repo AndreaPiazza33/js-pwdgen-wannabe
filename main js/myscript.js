8f9af6fd1ff75a216b90d5db246b10b4c1f7fd82
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-07-18 18:15:16 +0200

    feat: add js result

---
+++ main js/myscript.js
@@ -0,0 +1,8 @@
+let myname = window.prompt("Inserisci il tuo nome");
+let mysurname = window.prompt("Inserisci il tuo cognome");
+let mycolor = window.prompt("Inserisci il tuo colore preferito");
+let answers = myname + mysurname + mycolor + "21"
+
+
+console.log( typeof document.getElementById("my-password"));
+document.getElementById("my-password").innerHTML = answers
